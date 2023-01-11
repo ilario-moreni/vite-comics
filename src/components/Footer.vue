924d4d31e56a7dc456393887eb6cb7ebbba45014
ilario-moreni <ilario2001.mor@gmail.com>
2023-01-11 22:19:02 +0100

    footer (features section) first processing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -28,7 +28,117 @@ export default {
                     name:'DC POWER VISA',
                     url:'buy-dc-power-visa.svg'
                 }
-            ]
+            ],
+            features: {
+                dc_Comics_list: [
+                    {
+                        label:'Characters',
+                        link: '#'
+                    },
+                    {
+                        label:'Comics',
+                        link: '#'
+                    },
+                    {
+                        label:'Movies',
+                        link: '#'
+                    },
+                    {
+                        label:'TV',
+                        link: '#'
+                    },
+                    {
+                        label:'Games',
+                        link: '#'
+                    },
+                    {
+                        label:'Videos',
+                        link: '#'
+                    },
+                    {
+                        label:'News',
+                        link: '#'
+                    }
+                ],
+                shop_list: [
+                    {
+                        label:'Shop DC',
+                        link: '#'
+                    },
+                    {
+                        label:'Shop DC Collectibles',
+                        link: '#'
+                    }
+                ],
+                dc_list: [
+                    {
+                        label:'Terms Of Use',
+                        link: '#'
+                    },
+                    {
+                        label:'Privacy policy (New)',
+                        link: '#'
+                    },
+                    {
+                        label:'Ad Choices',
+                        link: '#'
+                    },
+                    {
+                        label:'Advertising',
+                        link: '#'
+                    },
+                    {
+                        label:'Jobs',
+                        link: '#'
+                    },
+                    {
+                        label:'Subscriptions',
+                        link: '#'
+                    },
+                    {
+                        label:'Talent Workshops',
+                        link: '#'
+                    },
+                    {
+                        label:'CPSC Certificates',
+                        link: '#'
+                    },
+                    {
+                        label:'Ratings',
+                        link: '#'
+                    },
+                    {
+                        label:'Shop Help',
+                        link: '#'
+                    },
+                    {
+                        label:'Contact Us',
+                        link: '#'
+                    }
+                ],
+                sites_list: [
+                    {
+                        label:'DC',
+                        link: '#'
+                    },
+                    {
+                        label:'MAD Magazine',
+                        link: '#'
+                    },
+                    {
+                        label:'DC Kids',
+                        link: '#'
+                    },
+                    {
+                        label:'DC Universe',
+                        link: '#'
+                    },
+                    {
+                        label:'DC Power Visa',
+                        link: '#'
+                    }
+                ]
+            }
         }
     }
 }
@@ -46,14 +156,58 @@ export default {
                 </ul>
             </div>
         </div>
-        <div class="main_container"></div>
+        <div class="features_container">
+            <div class="main_container features_lists_container">
+                <div>
+                    <ul class="features_ul">
+                        <li>
+                            <h3>DC COMICS</h3>
+                        </li>
+                        <li class="features_li" v-for="item in features.dc_Comics_list">
+                            {{ item.label }}
+                        </li>
+                    </ul>
+                    <ul class="features_ul">
+                        <li>
+                            <h3>SHOP</h3>
+                        </li>
+                        <li class="features_li" v-for="item in features.shop_list">
+                            {{ item.label }}
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="features_ul">
+                        <li>
+                            <h3>DC</h3>
+                        </li>
+                        <li class="features_li" v-for="item in features.dc_list">
+                            {{ item.label }}
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="features_ul">
+                        <li>
+                            <h3>SITES</h3>
+                        </li>
+                        <li class="features_li" v-for="item in features.sites_list">
+                            {{ item.label }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <div class="main_container"></div>
     </div>
 </template>
 
 <style lang="scss">
+
     @use './styles/partials/mixin.scss';
     @use './styles/partials/variables.scss' as *;
+
+    /* top footer container */
 
     .top_footer_container{
         background-color: $lightblue;
@@ -67,13 +221,14 @@ export default {
     .shop_ul{
         @include mixin.flat_ul;
         @include mixin.ul_style_none;
+        @include mixin.my_li;
         color: white;
         justify-content: space-between;
     }
 
     .shop_icon{
-        height: 70px;
-        width: 70px;
+        height: 60px;
+        width: 60px;
         object-fit: contain;
         margin-right: 10px;
     }
@@ -81,6 +236,36 @@ export default {
     .shop_li_label{
         display: flex;
         align-items: center;
+    }
+
+    /* features container */
+
+    .features_container{
+        background-image: url(../assets/img/footer-bg.jpg);
+        background-size: cover;
+        min-height: 200px;
+        max-height: 482px;
+    }
+
+    .features_lists_container{
+        display: flex;
+        margin: 0 auto;
+        padding: 2rem 0;
+        h3{
+            color:white;
+            margin: 1.5rem 0 1rem;
+        }
+    }
+
+    .features_ul{
+        @include mixin.ul_style_none;
+        width: 180px;
+    }
+
+    .features_li{
+        color: grey;
+        font-size: 14px;
+        line-height: 1.5rem;
     }
 
 </style>
